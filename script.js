2ac56bac90c182d1f812ad213486045b959696d5
Badricesor <badricesor9@gmail.com>
2024-07-06 18:40:26 +0530

    worked with buttons

---
+++ script.js
@@ -0,0 +1,10 @@
+// const submit= document.getElementById("downloadCv");
+
+// submit.addEventListener('click', function(){
+//     const fileUrl= "siva resume.dox"
+//     const fileName= "siva resume.dox"
+//     const link= document.createElement("a")
+//     link.href =fileUrl;
+//     link.download= fileName;
+//     link.click();
+// })
