8acdf88e00eee400239c9433c0671a2e34d432d8
Badricesor <badricesor9@gmail.com>
2024-07-07 17:12:19 +0530

    worked with responsiveness

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// const submit= document.getElementById("downloadCv");
 
-// submit.addEventListener('click', function(){
-//     const fileUrl= "siva resume.dox"
-//     const fileName= "siva resume.dox"
-//     const link= document.createElement("a")
-//     link.href =fileUrl;
-//     link.download= fileName;
-//     link.click();
-// })
